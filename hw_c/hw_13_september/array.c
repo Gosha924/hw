8f743d3d06679d01a2bb83c927a651a222e735df
Barinov Gosha <st139902@student.spbu.ru>
2025-10-01 21:31:54 +0300

    rework task

--- hw_c/hw_13_september/array.c
+++ hw_c/hw_13_september/array.c
@@ -2,23 +2,31 @@
 
 int main(void)
 {
-    /*
-    Моя программа переставляет массив чисел от 1 до 5 в массив 3 4 5 1 2
-    */
-    int array[] = {1, 2, 3, 4, 5 };
-    int zeroElem = array[0];
-    int firstElem = array[1];
-    int len = sizeof(array) / sizeof(array[0]);
-    for (int i = 0; i < len; i++) {
-        if (i < 3) {
-            array[i] = array[i + 2];
-        } else if (i == 3) {
-            array[i] = zeroElem;
-        } else {
-            array[i] = firstElem;
-        }
+    int array[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    printf("Введите длины отрезков n и m, учтите их общая длина должна быть 10\n");
+    unsigned int n, m;
+    scanf("%d", &n);
+    scanf("%d", &m);
+    if (n + m != 10) {
+        printf("Некорректный ввод\n");
+        return 0;
     }
-    for (int i = 0; i < len; i++) {
+    for (int i = 0; i < 5; i++) {
+        int elem = array[i];
+        array[i] = array[10 - 1 - i];
+        array[10 - 1 - i] = elem;
+    }
+    for (int i = 0; i < m / 2; i++) {
+        int elem = array[i];
+        array[i] = array[m - 1 - i];
+        array[m - 1 - i] = elem;
+    }
+    for (int j = 0; j < n / 2; j++) {
+        int elem = array[j + m];
+        array[j + m] = array[10 - 1 - j];
+        array[10 - 1 - j] = elem;
+    }
+    for (int i = 0; i < 10; i++) {
         printf("%d ", array[i]);
     }
     printf("\n");
