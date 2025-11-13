9a7df5950a80f02ffc0d7277d1b430135efed3dc
Barinov Gosha <st139902@student.spbu.ru>
2025-11-13 18:09:04 +0300

    переделал задачу поиск подстроки, переписав на цикл do ... while

--- hw_c/hw_19_september/string.c
+++ hw_c/hw_19_september/string.c
@@ -3,23 +3,25 @@
 
 int main(void)
 {
-    printf("Введите 2 строки, учтите максимальная длина строк 100 символов и строки разделяются по пробелу\n");
+    printf("Введите 2 строки, учтите максимальная длина строк 100 символов\n");
     char string[100];
-    char string1[100];
+    char substring[100];
     int count = 0;
     scanf("%s", string);
-    scanf("%s", string1);
-    if (strlen(string1) > strlen(string)) {
-        printf("%d\n", 0);
+    scanf("%s", substring);
+    if (strlen(substring) > strlen(string)) {
+        printf("0\n");
+        return 0;
     } else {
         char* inditorStr = string;
-        int len = strlen(string1);
-        inditorStr = strstr(inditorStr, string1);
-        while (inditorStr != NULL) {
-            count++;
-            inditorStr += 1;
-            inditorStr = strstr(inditorStr, string1);
-        }
+        int len = strlen(substring);
+        do {
+            inditorStr = strstr(inditorStr, substring);
+            if (inditorStr != NULL) {
+                count++;
+                inditorStr += 1;
+            }
+        } while (inditorStr != NULL);
     }
     printf("%d\n", count);
     return 0;
